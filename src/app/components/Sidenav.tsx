6b6a8cec3755aaa7fc71956bd1444046b1436ae2
Hammad <hammad.bashir0315@gmail.com>
2023-11-21 12:52:08 +0500

    tests folder disconnected

--- src/app/components/Sidenav.tsx
+++ src/app/components/Sidenav.tsx
@@ -25,13 +25,13 @@ const data = [
 
 const navData = [
   {
-    id:1,
+    id: 1,
     name: 'test',
-    href: '/tests',
+    href: '/',
     icon: <BookCheckIcon />
   },
   {
-    id:2,
+    id: 2,
     name: 'support',
     href: '/support',
     icon: <Grid2X2 />
@@ -47,21 +47,21 @@ function Sidenav() {
   return (
     <div className='flex'>
       <div className={` border   h-screen duration-300 w-20 md:w-72  p-5 pt-8 relative space-y-4 bg-slate-100`}>
-        
+
         <div className={` scale-0 md:scale-100  `}>
 
           <AccountForm languages={data} name={"Select Project"} />
         </div>
-    
+
         <ul className='flex flex-col gap-2 '>
           {navData.map(link =>
-           <Link href={link.href} key={link.id} className={classNames({
-            ' text-blue-500 bg-slate-200 rounded-md': link.href === currentPath,
-            'transition-all duration-300 ': link.href !== currentPath, 'hover:text-blue-500 hover:bg-slate-200  transition-colors' : true
-})} ><div className='flex items-center p-2 font-semibold gap-2 '><div className=''>{link.icon}</div><div className='scale-0 md:scale-100 duration-300 transition'>{link.name}</div></div> </Link>)}
-        
-      </ul>
-    </div>
+            <Link href={link.href} key={link.id} className={classNames({
+              ' text-blue-500 bg-slate-200 rounded-md': link.href === currentPath,
+              'transition-all duration-300 ': link.href !== currentPath, 'hover:text-blue-500 hover:bg-slate-200  transition-colors': true
+            })} ><div className='flex items-center p-2 font-semibold gap-2 '><div className=''>{link.icon}</div><div className='scale-0 md:scale-100 duration-300 transition'>{link.name}</div></div> </Link>)}
+
+        </ul>
+      </div>
     </div >
   )
 }
