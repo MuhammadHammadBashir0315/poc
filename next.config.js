780400907d3c6d9546a7427a4e8759f5a4a3a5a0
Hammad <hammad.bashir0315@gmail.com>
2023-11-26 14:21:11 +0500

    peranthesis are added in source

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
             {
                 // matching all API routes
                 // source: "/api/:path*",
-                "source": "/api/*",
+                "source": "/api/(*)",
                 headers: [
                     { key: "Access-Control-Allow-Credentials", value: "true" },
                     { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
